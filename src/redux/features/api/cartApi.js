3500edfaf8b1673bb804249400a9936e5d86cbee
Rownok Zahan Rupa <rownokzahanrupa@Rownoks-MacBook-Air.local>
2024-05-10 11:54:08 +0600

    Fix base url path issue

--- src/redux/features/api/cartApi.js
+++ src/redux/features/api/cartApi.js
@@ -1,46 +1,46 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = import.meta.env.API_BASE_URL;
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export const cartApi = createApi({
   reducerPath: "cartApi",
   baseQuery: fetchBaseQuery({ baseUrl }),
-  tagTypes: ["Cart"],
+  tagTypes: ["Carts"],
   endpoints: (build) => ({
     // Query to fetch all items from the cart by user ID
     getCartItemsByUserId: build.query({
       query: (userId) => ({
-        url: `/cart/${userId}`,
+        url: `/carts/${userId}`,
         method: "GET",
       }),
     }),
 
     // Mutation to add an item to the cart
     addToCart: build.mutation({
-      query: (item) => ({
-        url: "/cart",
+      query: ({ userId, item }) => ({
+        url: `/carts/${userId}/items`,
         method: "POST",
         body: item,
       }),
-      invalidatesTags: ["Cart"],
+      invalidatesTags: ["Carts"],
     }),
 
     // Mutation to remove an item from the cart
     removeFromCart: build.mutation({
-      query: (itemId) => ({
-        url: `/cart/${itemId}`,
+      query: ({ userId, itemId }) => ({
+        url: `/carts/${userId}/items/${itemId}`,
         method: "DELETE",
       }),
-      invalidatesTags: ["Cart"],
+      invalidatesTags: ["Carts"],
     }),
 
     // Mutation to clear the entire cart
     clearCart: build.mutation({
-      query: () => ({
-        url: "/cart/clear",
+      query: (userId) => ({
+        url: `/carts/${userId}/clear`,
         method: "DELETE",
       }),
-      invalidatesTags: ["Cart"],
+      invalidatesTags: ["Carts"],
     }),
   }),
 });
--- src/redux/features/api/userApi.js
+++ src/redux/features/api/userApi.js
@@ -1,6 +1,6 @@
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
-const baseUrl = import.meta.env.API_BASE_URL;
+const baseUrl = import.meta.env.VITE_API_BASE_URL;
 
 export const userApi = createApi({
   reducerPath: "userApi",
