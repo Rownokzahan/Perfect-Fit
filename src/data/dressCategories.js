c928d812b37170b49eda9dcbced101fd78562a60
Rownok Zahan Rupa <rownokzahanrupa@Rownoks-MacBook-Air.local>
2024-05-19 11:52:12 +0600

    Add extra info

--- src/data/dressCategories.js
+++ src/data/dressCategories.js
@@ -1,9 +1,9 @@
 const dressCategories = [
-  "All Dress",
-  "Seasonal Dress",
-  "Formal Gown",
-  "Maxi Dress",
-  "Cocktail Dress",
+  { _id: "1", name: "All Dress", slug: "all-dress" },
+  { _id: "2", name: "Seasonal Dress", slug: "seasonal-dress" },
+  { _id: "3", name: "Formal Gown", slug: "formal-gown" },
+  { _id: "4", name: "Maxi Dress", slug: "maxi-dress" },
+  { _id: "5", name: "Cocktail Dress", slug: "cocktail-dress" },
 ];
 
 export default dressCategories;
--- src/data/dressSortOptions.js
+++ src/data/dressSortOptions.js
@@ -1,10 +1,10 @@
 const dressSortOptions = [
-  "Newest",
-  "Best Selling",
-  "Price Low to High",
-  "Price High to Low",
-  "A - Z",
-  "Z - A",
+  { _id: "1", name: "Newest", slug: "newest" },
+  { _id: "2", name: "Best Selling", slug: "best-selling" },
+  { _id: "3", name: "Price Low to High", slug: "price-low-to-high" },
+  { _id: "4", name: "Price High to Low", slug: "price-high-to-low" },
+  { _id: "5", name: "A - Z", slug: "a-z" },
+  { _id: "6", name: "Z - A", slug: "z-a" },
 ];
 
 export default dressSortOptions;
